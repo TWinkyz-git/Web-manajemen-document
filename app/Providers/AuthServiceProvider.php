93783408f1e1804686caa2430778ee9a1d3bf3e4
Twinkyz-git <randwikaz@gmail.com>
2026-06-27 01:36:04 +0700

    feat: add team controller with authorization and member management

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Document;
+use App\Models\Team;
+use App\Policies\DocumentPolicy;
+use App\Policies\TeamPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Document::class => DocumentPolicy::class,
+        Team::class => TeamPolicy::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
